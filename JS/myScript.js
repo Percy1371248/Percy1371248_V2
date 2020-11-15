0a967e1143f9edb1b0e8fb2c93cba7b3c1317fe6
KidPercy <mthimkhulupercy3@mail.com>
2020-11-16 01:03:19 +0200

    Final stage

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,4 +1,4 @@
-
+//back and forward functions
 function BackFunction() {
   document.getElementById("myBack");
   window.history.back();
@@ -9,22 +9,20 @@ function ForwardFunction() {
  window.history.forward();
 }
 
+//Mobile Menu Navigation
 function onClickMenu(){
 	document.getElementById("menu").classList.toggle("change");
 	document.getElementById("Mobilenav").classList.toggle("change");
 	
 }
 
-//Get the button
 
-// When the user scrolls down 20px from the top of the document, show the button
+//To Top function
 window.onscroll = function() {scrollFunction()};
-
 
 function scrollFunction() {
   var mybutton = document.getElementById("myTbtn");
 	
-
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ) {
     mybutton.style.display = "block";
   } else {
@@ -33,7 +31,6 @@ function scrollFunction() {
     
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
 	document.body.scrollTop = 0; // For Safari
 	document.documentElement.scrollTop = 0;
@@ -41,17 +38,13 @@ function topFunction() {
 
 
 
-//Gallery Tab
+// Illustration Gallery 
 function myFunction(imgs) {
-  // Get the expanded image
+
   var expandImg = document.getElementById("expandedImg");
-  // Get the image text
   var imgText = document.getElementById("imgtext");
-  // Use the same src in the expanded image as the image being clicked on from the grid
   expandImg.src = imgs.src;
-  // Use the value of the alt attribute of the clickable image as text inside the expanded image
   imgText.innerHTML = imgs.alt;
-  // Show the container element (hidden with CSS)
   expandImg.parentElement.style.display = "block";
 }
 
